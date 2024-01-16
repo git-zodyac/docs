66c849f68f0026b3b8994a015458a015bb11f273
apiliugin <apiliugin@quakeglobal.com>
2024-01-16 01:03:31 +0100

    Google analytics module

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -15,12 +15,18 @@ import {
 } from '@ngneat/transloco';
 import { HIGHLIGHT_OPTIONS } from 'ngx-highlightjs';
 import { PageSkeleton } from './ui/page-skeleton/page-skeleton.component';
+import {
+  NgxGoogleAnalyticsModule,
+  NgxGoogleAnalyticsRouterModule,
+} from 'ngx-google-analytics';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimations(),
     importProvidersFrom(HttpClientModule),
+    importProvidersFrom(NgxGoogleAnalyticsModule.forRoot('G-4FT4VBVSJF')),
+    importProvidersFrom(NgxGoogleAnalyticsRouterModule),
     provideTranslocoLoadingTpl(PageSkeleton),
     provideHttpClient(),
     provideTransloco({
